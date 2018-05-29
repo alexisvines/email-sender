dfdec9909326cd560f005d8c13c96d9b7cd193d8
Alexis Freire <Alexis.freire.leiva@gmail.com>
2018-05-29 01:40:30 -0400

    Avance

---
+++ src/main/java/emailsender/Mail.java
@@ -0,0 +1,12 @@
+package emailsender;
+
+import lombok.Data;
+
+@Data
+public class Mail {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String content;
+}
